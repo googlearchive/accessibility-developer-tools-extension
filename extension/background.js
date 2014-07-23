f0aa91b35ea7f97ac0f9f85e05903f68505594db
Alice Boxhall <aboxhall@chromium.org>
2014-07-23 13:58:02 -0700

    Add comments etc.

--- extension/background.js
+++ extension/background.js
@@ -35,8 +35,8 @@ chrome.extension.onRequest.addListener(
         case 'injectContentScripts':
             var tabId = request.tabId;
             var framesLoaded = {};
-            var framesInjected = null;
             injectContentScripts(tabId, callback);
+            var framesInjected = [];
             if (inspectedTabs.indexOf(tabId) == -1) {
                 chrome.webNavigation.onBeforeNavigate.addListener(
                     function(details) {
@@ -45,21 +45,26 @@ chrome.extension.onRequest.addListener(
                             framesInjected = null;
                         }
                     });
-                chrome.webNavigation.onDOMContentLoaded.addListener(
-                    function(details) {
-                        framesLoaded[details.frameId] = true;
-                    });
                 chrome.webNavigation.onCompleted.addListener(
                     function(details) {
                         if (details.tabId != tabId)
                             return;
 
                         if (details.frameId == 0) {
+                            // When the top frame completes loading, inject content scripts into all
+                            // frames. Copy the list of all frames seen so far into |framesInjected|
                             injectContentScripts(tabId);
                             framesInjected = Object.keys(framesLoaded);
-                        } else if (framesInjected && framesInjected.indexOf(details.frameId) == -1) {
+                        } else if (framesInjected &&
+                                   framesInjected.indexOf(String(details.frameId)) == -1) {
+                            // If a frame completes loading after the top frame, we need to inject
+                            // content scripts into all frames again, so that we catch this one.
                             injectContentScripts(tabId);
-                            framesInjected.push(details.frameId);
+                            framesInjected.push(String(details.frameId));
+                        } else {
+                            // If a frame completes loading before the top frame, keep track of it
+                            // so we know what frames we're injecting content scripts into.
+                            framesLoaded[details.frameId] = true;
                         }
                     });
             }
