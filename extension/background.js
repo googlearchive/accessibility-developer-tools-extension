36fbe6497a5080761000b3aa7f698f9e11b792d0
Alice Boxhall <aboxhall@chromium.org>
2014-08-04 12:10:49 -0700

    Fix issue where callback was not being called when all content scripts were injected; remove logspam

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,8 @@
-var inspectedTabs = [];
+var inspectedTabs = {};
 var contentScriptTokens = {};
 
-function injectContentScript(tabId, remaining_scripts, token, opt_callback) {
+function injectContentScript(tabId, remaining_scripts, token) {
     if (token != contentScriptTokens[tabId]) {
-        console.log('out of date token:', token, 'does not match', contentScriptTokens[tabId]);
         return;
     }
     var script = remaining_scripts.shift();
@@ -12,29 +11,30 @@ function injectContentScript(tabId, remaining_scripts, token, opt_callback) {
         { file: script, allFrames: true },
         function() {
             if (chrome.extension.lastError) {
-                if (opt_callback)
-                    opt_callback({ error: chrome.extension.lastError.message });
+                if ('callback' in inspectedTabs[tabId]) {
+                    var callback = inspectedTabs[tabId].callback;
+                    callback({ error: chrome.extension.lastError.message });
+                    delete inspectedTabs[tabId].callback;
+                }
                 return;
             }
             if (remaining_scripts.length)
-                injectContentScript(tabId, remaining_scripts, token, opt_callback);
-            else if (opt_callback)
-                opt_callback({ success: true });
+                injectContentScript(tabId, remaining_scripts, token);
+            else if ('callback' in inspectedTabs[tabId]) {
+                var callback = inspectedTabs[tabId].callback;
+                callback({ success: true });
+                delete inspectedTabs[tabId].callback;
+            }
         });
 };
 
-function injectContentScripts(tabId, opt_callback) {
-chrome.tabs.executeScript(tabId, { code: 'console.log("about to inject scripts");' });
-    console.log('injecting content scripts');
+function injectContentScripts(tabId) {
     if (!(tabId in contentScriptTokens)) {
-        console.log(tabId, 'not in', contentScriptTokens);
         contentScriptTokens[tabId] = 0;
     } else {
-        console.log(tabId, 'in', contentScriptTokens, '; incrementing');
         contentScriptTokens[tabId]++;
     }
     var token = contentScriptTokens[tabId];
-    console.log('token', token);
     var scripts = [ 'generated/axs.js',
                     'generated/constants.js',
                     'generated/utils.js',
@@ -42,7 +42,7 @@ chrome.tabs.executeScript(tabId, { code: 'console.log("about to inject scripts")
                     'generated/audits.js',
                     'generated/extension_properties.js',
                     'generated/extension_audits.js' ]
-    injectContentScript(tabId, scripts, token, opt_callback);
+    injectContentScript(tabId, scripts, token);
 }
 
 chrome.extension.onRequest.addListener(
@@ -50,9 +50,8 @@ chrome.extension.onRequest.addListener(
         switch(request.command) {
         case 'injectContentScripts':
             var tabId = request.tabId;
-            injectContentScripts(tabId, callback);
             var topFrameLoaded = true;
-            if (inspectedTabs.indexOf(tabId) == -1) {
+            if (!(tabId in inspectedTabs)) {
                 chrome.webNavigation.onBeforeNavigate.addListener(
                     function(details) {
                         if (details.tabId == tabId && details.frameId == 0) {
@@ -63,23 +62,20 @@ chrome.extension.onRequest.addListener(
                     function(details) {
                         if (details.tabId != tabId)
                             return;
-                        console.log('frame', details.frameId, 'completed loading');
                         if (details.frameId == 0) {
-                            console.log('page reloaded; injecting content scripts');
                             // When the top frame completes loading, inject content scripts into all
                             // frames. Copy the list of all frames seen so far into |framesInjected|
                             injectContentScripts(tabId);
                             topFrameLoaded = true;
                         } else if (topFrameLoaded) {
-                            console.log('frame', details.frameId, details.url,
-                                        'completed after top frame; re-injecting content scripts');
                             // If a frame completes loading after the top frame, we need to inject
                             // content scripts into all frames again, so that we catch this one.
                             injectContentScripts(tabId);
                         }
                     });
             }
-            inspectedTabs.push(tabId);
+            inspectedTabs[tabId] = { callback: callback };
+            injectContentScripts(tabId);
             return;
         case 'getAuditPrefs':
             chrome.storage.sync.get('auditRules', callback);
